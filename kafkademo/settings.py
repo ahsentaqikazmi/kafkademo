c366f4aee001a1be3f164cde12333b96efbc3b9d
ahsentaqikazmi <m.ahsen@inboxbiz.com>
2017-08-27 01:24:34 +0500

    testing with nginx

--- kafkademo/settings.py
+++ kafkademo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f7_+lt&t=kn&ibqmevuqgk&=vcm&hqh6h*o!afvm__lp@dsa!+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '52.26.180.46']
 
 
 # Application definition
